636396325ded3dbf51f2cb0cdfb76ed7e6a7c6f2
Olesia Zaidman <123760026+olesiaZaidman@users.noreply.github.com>
2023-05-31 19:44:27 +0100

    Exported

--- Assets/Script/Dropper.cs
+++ Assets/Script/Dropper.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.WSA;
+
 
 public class Dropper : MonoBehaviour
 {
